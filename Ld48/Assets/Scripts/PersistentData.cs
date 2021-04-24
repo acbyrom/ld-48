192a3e71c6b3acd5cc4f3c945f4e122948d9a2eb
Btdyaya <billytdavies@gmail.com>
2021-04-24 19:58:19 +0100

    some things

---
+++ Ld48/Assets/Scripts/PersistentData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentData : MonoBehaviour
+{
+    static float volume=100f;
+    static float sensitivity=100f;
+    static int roomNo=0;
+    static int floorNo=0;
+
+}
