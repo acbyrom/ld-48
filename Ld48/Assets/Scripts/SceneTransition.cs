1d825dbf257867e0c606708c09499e73ad145c85
acbyrom <82965048+acbyrom@users.noreply.github.com>
2021-04-26 17:30:50 +0100

    Scenes

--- Ld48/Assets/Scripts/SceneTransition.cs
+++ Ld48/Assets/Scripts/SceneTransition.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    string[] scenes = {"Room1","Room2","Room4","Room5", "Room6", "Room7", "Room8", "Room9", "Room10", "Room11", "Room12", "Room13", "Room14", "Room15", "Room16" };
+    string[] scenes = {"Room1","Room2", "Room3", "Room4","Room5", "Room6", "Room7", "Room8", "Room9", "Room10", "Room11", "Room12", "Room13", "Room14", "Room15", "Room16", "Room17", "Room18", "Room19" };
     public void Die(){
         StartCoroutine(ReloadLevel());
     }
